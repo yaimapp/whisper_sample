8d7c77af5390a51cd3aa0c614d9ee11d063458c8
yaimapp <gmonday730-mbp@yahoo.co.jp>
2023-05-01 15:30:32 +0900

    API KEYは環境変数から

--- whisper.sh
+++ whisper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 FILE=$1
 curl https://api.openai.com/v1/audio/transcriptions \
-  -H "Authorization: Bearer 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" \
+  -H "Authorization: Bearer $OPENAI_API_KEY" \
   -H "Content-Type: multipart/form-data" \
   -F response_format="text" \
   -F model="whisper-1" \

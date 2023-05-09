beebaf12912db5ebfe02936225c1d0d29fdd449a
yaimapp <gmonday730-mbp@yahoo.co.jp>
2023-05-09 20:42:11 +0900

    レスポンスのオプションを変更

--- whisper.sh
+++ whisper.sh
@@ -3,6 +3,6 @@ FILE=$1
 curl https://api.openai.com/v1/audio/transcriptions \
   -H "Authorization: Bearer $OPENAI_API_KEY" \
   -H "Content-Type: multipart/form-data" \
-  -F response_format="text" \
+  -F response_format="verbose_json" \
   -F model="whisper-1" \
   -F file="@$FILE"
